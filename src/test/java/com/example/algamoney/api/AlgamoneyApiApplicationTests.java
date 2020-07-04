47d082a7a9d1937c6c47c29c938203471c48f7c8
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-04 14:25:53 -0300

    Criação, migração da tabela categoria, e inciando a implementação do jpa

---
+++ src/test/java/com/example/algamoney/api/AlgamoneyApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.algamoney.api;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoneyApiApplicationTests {
+
+	
+	void contextLoads() {
+	}
+
+}
