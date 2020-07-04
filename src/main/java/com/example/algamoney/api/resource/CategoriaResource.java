fb21642c03881494744cf1ffb0c1a602ac7f5354
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-04 18:22:48 -0300

    Retornando no corpo da resposta os dados do recurso que foi criado via POST

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -6,12 +6,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -32,16 +32,21 @@ public class CategoriaResource {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
 		
 	Categoria categoriaJaSalva = categoriaRepository.save(categoria);
 	
 	URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaJaSalva.getCodigo()).toUri();
 	
 	response.setHeader("Location", uri.toASCIIString());
+	
+	return ResponseEntity.created(uri).body(categoriaJaSalva);
 	}
 	
+	@RequestMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable long codigo) {
+		return categoriaRepository.findOne(codigo);
+	}
 	
 	
 	
