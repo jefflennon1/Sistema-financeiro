e51848aed19e0e7539375e715cdfb60f895f2363
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-04 16:20:58 -0300

    Configurando o header de resposta para mostrar o LOCATION ao submeter um post

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,19 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
@@ -22,4 +30,19 @@ public class CategoriaResource {
 		
 		return categoriaRepository.findAll();
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		
+	Categoria categoriaJaSalva = categoriaRepository.save(categoria);
+	
+	URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaJaSalva.getCodigo()).toUri();
+	
+	response.setHeader("Location", uri.toASCIIString());
+	}
+	
+	
+	
+	
 }
