e8b7c46237060fe732f422589cbfd0f175d279c4
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-06 16:26:46 -0300

    Melhorando o bean validation

--- src/main/java/com/example/algamoney/api/model/Categoria.java
+++ src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name="categoria")
@@ -17,6 +18,7 @@ public class Categoria {
 	
 	
 	@NotNull
+	@Size(min=3, max=20)
 	private String nome;
 
 	public long getCodigo() {
