3ba6a6e42d7d2395c9b5621fffea9987bc7836f1
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-27 20:30:54 -0300

    salvando logo o escopo dos métodos que vão ser usados

--- src/main/java/com/example/algamoney/api/repository/categoria/CategoriaRepositoryImp.java
+++ src/main/java/com/example/algamoney/api/repository/categoria/CategoriaRepositoryImp.java
@@ -1,5 +1,13 @@
 package com.example.algamoney.api.repository.categoria;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -7,12 +15,45 @@ import org.springframework.data.domain.Pageable;
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.filter.CategoriaFilter;
 
+
+
 public class CategoriaRepositoryImp implements CategoriaQuery {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
+	
 	@Override
 	public Page<Categoria> filtrar(CategoriaFilter categoriaFilter, Pageable pageable) {
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<Categoria> criteria = builder.createQuery(Categoria.class);
+		Root<Categoria> root = criteria.from(Categoria.class);
+		 
+		Predicate[] predicates = criarRestricoes(categoriaFilter, builder, root);
+		criteria.where(predicates);
 		
-		return new PageImpl<Categoria>(content, pageable, total(categoriaFilter));
+		TypedQuery<Categoria> query = manager.createQuery(criteria);
+		criarPaginacao(query, pageable);
+		return new PageImpl<>(query.getResultList(), pageable, total(categoriaFilter));
+	}
+ 
+
+	private void criarPaginacao(TypedQuery<Categoria> query, Pageable pageable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	private Long total(CategoriaFilter categoriaFilter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	private Predicate[] criarRestricoes(CategoriaFilter categoriaFilter, CriteriaBuilder builder,
+			Root<Categoria> root) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
