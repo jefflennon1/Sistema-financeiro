69b94783251e585f2dded845024e749b31df7deb
jefferson lennon <jeffersonlennon1@outlook.com>
2020-07-30 21:42:17 -0300

    Finalizando configuração inicial do meu access token

--- src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/algamoney/api/config/AuthorizationServerConfig.java
@@ -1,11 +1,15 @@
 package com.example.algamoney.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -24,4 +28,14 @@ public class AuthorizationServerConfig  extends AuthorizationServerConfigurerAda
 		.accessTokenValiditySeconds(1800);
 	}
 	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints.tokenStore(tockenStore())
+		.authenticationManager(authenticationManager);
+	}
+
+	@Bean
+	public TokenStore tockenStore() {
+		return new InMemoryTokenStore();
+	}
 }
