ce2a615967932a25016cb324187960ac033d9118
jefferson lennon <jeffersonlennon1@outlook.com>
2020-08-09 15:26:39 -0300

    Pegando o response

--- src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/example/algamoney/api/token/RefreshTokenPostProcessor.java
@@ -1,6 +1,7 @@
 package com.example.algamoney.api.token;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
@@ -8,6 +9,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
@@ -26,6 +28,7 @@ public class RefreshTokenPostProcessor  implements ResponseBodyAdvice<OAuth2Acce
 			ServerHttpRequest request, ServerHttpResponse response) {
 		
 		HttpServletRequest req = ((ServletServerHttpRequest)request).getServletRequest();
+		HttpServletResponse resp = ((ServletServerHttpResponse)response).getServletResponse();
 		
 		String refreshToken = body.getRefreshToken().getValue();
 		adicionarRefreshTokenNoCookie(refreshToken);
